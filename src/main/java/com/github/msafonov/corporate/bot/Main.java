fd1ec729b7272ab74c6a2cf824245255658b7833
hundwuff1 <kamitrushkin@gmail.com>
2021-06-30 11:47:17 +0300

    Добавлено взаимодействие с Telegram API добавлено чтение и работа с файлом конфигураций обновлен build.gradle исравлены мелкие недочеты

--- src/main/java/com/github/msafonov/corporate/bot/Main.java
+++ src/main/java/com/github/msafonov/corporate/bot/Main.java
@@ -1,10 +1,19 @@
 package com.github.msafonov.corporate.bot;
 
+import java.sql.SQLException;
+
 /**
  * @author Mike Safonov
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Migration migration = new Migration("jdbc:postgresql://localhost:5432/test_db", "postgres", "postgres", "public");
+        migration.migrateToNewVersion();
 
+        CreateBot createBot =new CreateBot();
     }
 }
+
+
+
+
